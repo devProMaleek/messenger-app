a0ba9286ea964645d2f99d738ca4a54022da5f24
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-04 01:58:49 +0100

    feat: Add the toaster context for toaster

---
+++ app/context/ToasterContext.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { Toaster } from 'react-hot-toast';
+
+const ToasterContext = () => {
+  return <Toaster position="top-right" reverseOrder={false} />;
+};
+
+export default ToasterContext;
