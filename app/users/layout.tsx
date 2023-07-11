91e4a07d8ed25e4cdd1e670db9695b42bb18e5c0
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-11 23:57:41 +0100

    feat: Create the Layout component.
    1. Create the layout for the main screen of the user page.

---
+++ app/users/layout.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+import Sidebar from '../components/Sidebar/Sidebar';
+
+export const metadata: Metadata = {
+  title: 'Users',
+  description: 'SEO Title',
+};
+export default async function UsersLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full">
+      {/* @ts-expect-error Server Component */}
+      <Sidebar>{children}</Sidebar>
+    </div>
+  );
+}
