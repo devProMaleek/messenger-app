b88acb1e9a7c36be4c49bffd419ec912f39539d7
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-22 23:59:33 +0100

    feat: Add the function to the layout page and pass it to a client component

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,20 @@
 import { Metadata } from 'next';
 import Sidebar from '../components/Sidebar/Sidebar';
+import getUsers from '../actions/getUsers';
+import UserList from './components/UserList';
 
 export const metadata: Metadata = {
   title: 'Users',
   description: 'SEO Title',
 };
 export default async function UsersLayout({ children }: { children: React.ReactNode }) {
+  const users = await getUsers();
   return (
-    <div className="h-full">
-      <Sidebar>{children}</Sidebar>
-    </div>
+    <Sidebar>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
+    </Sidebar>
   );
 }
