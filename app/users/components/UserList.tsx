14917ac8bee311b104877f35990f8673921d22c2
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-07-23 08:42:28 +0100

    feat: Finish the friends sections
    Show the friends of the logged in user

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -1,22 +1,24 @@
 'use client';
 
 import { User } from '@prisma/client';
+import UserBox from './UserBox';
 
 interface UserListProps {
   items: User[];
 }
-export default function UserList({items}: UserListProps) {
+export default function UserList({ items }: UserListProps) {
   return (
     <>
-    <aside className="fixed inset-y-0 left-0 block w-full pb-20 overflow-y-auto border-r border-gray-200 lg:pb-0 lg:left-20 lg:w-20 lg:block">
-      <div className="px-5">
-        <div className="flex-col">
-          <div className="py-4 text-2xl font-bold text-neutral-800">
-            Friends
+      <aside className="fixed inset-y-0 left-0 block w-full pb-20 overflow-y-auto border-r border-gray-200 lg:pb-0 lg:left-20 lg:w-80 lg:block">
+        <div className="px-5">
+          <div className="flex-col">
+            <div className="py-4 text-2xl font-bold text-neutral-800">Friends</div>
           </div>
+          {items.map((item) => (
+            <UserBox key={item.id} data={item} />
+          ))}
         </div>
-      </div>
-    </aside>
+      </aside>
     </>
   );
 }
